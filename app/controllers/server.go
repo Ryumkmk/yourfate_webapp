756eab1321ef74e0672cba632fe3cdbf7a83ae11
Ryumkmk <luihao1999929@icloud.com>
2023-03-31 16:13:29 +0900

    port_heroku

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 	"yourfate_webapp/config"
 )
 
@@ -22,5 +23,7 @@ func StartMainServer() error {
 	http.Handle("/static/", http.StripPrefix("/static/", files))
 	http.HandleFunc("/", top)
 	http.HandleFunc("/fate", fate)
-	return http.ListenAndServe(":"+config.Config.Port, nil)
+
+	port := os.Getenv("PORT")
+	return http.ListenAndServe(":"+port, nil)
 }
